e2423ae6910f7ed41c18ce0990db94fdba2328d6
Alan <alan89757@163.com>
2024-09-16 19:42:09 +0800

    正常运行v3

--- entry/src/main/cpp/PackageProvider.cpp
+++ entry/src/main/cpp/PackageProvider.cpp
@@ -1,6 +1,8 @@
 #include "RNOH/PackageProvider.h"
 #include "DateTimePickerPackage.h"
 #include "SafeAreaViewPackage.h"
+#include "PdfViewPackage.h"
+// #include "ReanimatedPackage.h"
 // #include "AsyncStoragePackage.h"
 // #include "LinearGradientPackage.h"
 
@@ -10,6 +12,8 @@ std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Cont
     return {
         std::make_shared<DateTimePickerPackage>(ctx),
         std::make_shared<SafeAreaViewPackage>(ctx),
+        std::make_shared<PdfViewPackage>(ctx),
+//         std::make_shared<ReanimatedPackage>(ctx),
 //         std::make_shared<LinearGradientPackage>(ctx)
 //         std::make_shared<AsyncStoragePackage>(ctx)
     };
